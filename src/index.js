de81ff902dd5316139ce22c875bb5d4db7255415
andela-hms <humphrey.musonye@andela.com>
2017-10-04 17:15:38 +0300

    create searchbar logic

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const YOUTUBE_API_KEY = 'AIzaSyB0_07uMsqs33LLbjBbQaJqoRJwR26Ugtw';
+
 // create a new component
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+  <div>
+    <SearchBar />
+  </div>
+  );
 }
 // take the generated html and place it in the DOM
 ReactDOM.render(<App />, document.querySelector('.container'));
